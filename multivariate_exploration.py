40cb1461844688d2de44fe44238c072a1beab64a
Liam-S-Sweeney <liam.s.sweeney@outlook.com>
2026-03-03 18:25:21 -0500

    multivar cleanup

--- multivariate_exploration.py
+++ multivariate_exploration.py
@@ -3,202 +3,91 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import scipy.stats as stats
+from pathlib import Path
 from data_loader import load_clean
 from config import HUE_COL, SIZE_COL, PALETTE
-
+from cdfs import compute_descriptives_for_series
 
 def explore_multi_variables(*cols, palette=PALETTE):
-
-    clean_df = load_clean() 
-
-    descriptive_dict = {
-        # Variable Info
-        'position': [],
-        'variable_name': [],
-
-        # Measures of Central Tendency
-        'mean': [],
-        'median': [],
-        'mode': [],
-
-        # Measures of Variability
-        'var': [],
-        'std': [],
-        'range': [],
-        'q1': [],
-        'q3': [],
-        'iqr': [],
-
-        # Measures of Shape
-        'skew': [],
-        'kurtosis': [],
-
-        # Measures of Frequency
-        'count': [],
-        'frequency': [],
-
-        # Misc
-        'min': [],
-        'max': [],
-        'se_mean': [],
-    }
+    df = load_clean()
+    descriptive_dict = [
+        compute_descriptives_for_series(df[col], name=col, position=df.columns.get_loc(col))
+        for col in cols
+        ]
+    
+    out_dir = Path('multivariate_analysis')
+    cols_title = '-'.join(cols)
+    pd.DataFrame(descriptive_dict).to_csv(out_dir / f"{cols_title}-multivar_analysis.csv",index=False)
 
     for col in cols:
-        var_col = clean_df[col]
-        non_na = var_col.dropna()
-        values = non_na.to_numpy(dtype=float)
-        non_na_array = np.array(non_na)
-
-        descriptive_dict['position'].append(clean_df.columns.get_loc(col))
-        descriptive_dict['variable_name'].append(col)
-
-        if non_na.empty:
-            descriptive_dict['mean'].append(np.nan)
-            descriptive_dict['median'].append(np.nan)
-            descriptive_dict['mode'].append(np.nan)
-
-            descriptive_dict['var'].append(np.nan)
-            descriptive_dict['std'].append(np.nan)
-            descriptive_dict['range'].append(np.nan)
-            descriptive_dict['q1'].append(np.nan)
-            descriptive_dict['q3'].append(np.nan)
-            descriptive_dict['iqr'].append(np.nan)
-
-            descriptive_dict['skew'].append(np.nan)
-            descriptive_dict['kurtosis'].append(np.nan)
-
-            descriptive_dict['count'].append(np.nan)
-            descriptive_dict['frequency'].append(np.nan)
-
-            descriptive_dict['min'].append(np.nan)
-            descriptive_dict['max'].append(np.nan)
-            descriptive_dict['se_mean'].append(np.nan)
-        
-        # Descriptive Computations
-        else:
-            # Measures of Central Tendency
-            descriptive_dict['mean'].append(non_na.mean())
-            descriptive_dict['median'].append(non_na.median())
-            descriptive_dict['mode'].append(stats.mode(non_na_array, axis=None, keepdims=False))
-
-            # Measures of Variability
-            descriptive_dict['var'].append(non_na.var())
-            descriptive_dict['std'].append(non_na.std())
-            descriptive_dict['range'].append(non_na.max()-non_na.min())
-            descriptive_dict['q1'].append(np.percentile(non_na, 25))
-            descriptive_dict['q3'].append(np.percentile(non_na, 75))
-            descriptive_dict['iqr'].append(np.percentile(non_na, 75)-np.percentile(non_na, 25))
-
-            # Measures of Shape
-            if values.size < 3 or np.isclose(values.std(ddof=0), 0.0, atol=1e-8):
-                descriptive_dict['skew'].append(np.nan)
-                descriptive_dict['kurtosis'].append(np.nan)    
-            else:
-                descriptive_dict['skew'].append(stats.skew(np.array(list(non_na))))
-                descriptive_dict['kurtosis'].append(stats.kurtosis(np.array(list(non_na))))
-            
-            # Measures of Frequency
-            n = non_na_array.size
-            unique_values, counts = np.unique(non_na_array, return_counts=True)
-
-            n_unique = len(counts)
-
-            if n_unique > 20:
-                frequency = "CONTINUOUS"
-            else:
-                percents = np.round((counts / n) * 100, 2)
-                frequency = [(float(uv), int(ct), float(p)) for uv, ct, p in zip(unique_values, counts, percents)] 
-
-                    
-
-            # Measures of Frequency | n; unique_val, count, percent
-            descriptive_dict['count'].append(n)
-            descriptive_dict['frequency'].append(frequency)
-
-            # Misc
-            descriptive_dict['min'].append(non_na.min())
-            descriptive_dict['max'].append(non_na.max())
-            descriptive_dict['se_mean'].append(non_na.std() / np.sqrt(n))
+        # non_na df of cols
+        plot_vals = df[col]
         
         # attempt to add a palette
-            try:
-                sns.color_palette(palette, as_cmap=True)
-            except:
-                pass
-        
+        try:
+            sns.color_palette(palette, as_cmap=True)
+        except:
+            pass
+
         # Shape of Distribution per Variable
-            fig, (ax_probplot, ax_hist, ax_box, ax_violin, ax_swarm) = plt.subplots(1,5, figsize=(16, 6))
+        fig, (ax_probplot, ax_hist, ax_box, ax_violin, ax_swarm) = plt.subplots(1,5, figsize=(16, 6))
 
-            # Probability Plot
-            stats.probplot(
-                non_na,
-                dist='norm',
-                plot=ax_probplot
-                )
-            
-            points, line = ax_probplot.get_lines()
+        # Probability Plot
+        stats.probplot(
+            plot_vals,
+            dist='norm',
+            plot=ax_probplot
+            )
+        
+        points, line = ax_probplot.get_lines()
 
-            # Style the Points
-            points.set_color("steelblue")
-            points.set_marker("o")
-            points.set_markersize(5)
-            points.set_alpha(0.7)
+        # Style the Points
+        points.set_color("steelblue")
+        points.set_marker("o")
+        points.set_markersize(5)
+        points.set_alpha(0.7)
 
-            # Style the fit line
-            line.set_color("red")
-            line.set_linewidth(2)
-            line.set_linestyle("-")
+        # Style the fit line
+        line.set_color("red")
+        line.set_linewidth(2)
+        line.set_linestyle("-")
 
-            ax_probplot.set_title(f'Probability Plot')
+        ax_probplot.set_title(f'Probability Plot')
 
-            # Histogram
-            mu, sigma = stats.norm.fit(non_na) 
-            counts, bins, _ = ax_hist.hist(
-                non_na,
-                bins='fd',
-                edgecolor='black',
-                alpha=0.6,
-                )
-            x_dense = np.linspace(non_na.min(), non_na.max(), 500)
-            pdf_dense = stats.norm.pdf(x_dense, mu, sigma)
-            pdf_dense_scaled = pdf_dense * len(non_na) * (bins[1] - bins[0])
+        # Histogram
+        plot_vals = plot_vals[np.isfinite(plot_vals)]
+        mu, sigma = stats.norm.fit(plot_vals)
+        counts, bins, _ = ax_hist.hist(
+            plot_vals,
+            bins='fd',
+            edgecolor='black',
+            alpha=0.6,
+            )
+        x_dense = np.linspace(plot_vals.min(), plot_vals.max(), 500)
+        pdf_dense = stats.norm.pdf(x_dense, mu, sigma)
+        pdf_dense_scaled = pdf_dense * len(plot_vals) * (bins[1] - bins[0])
 
-            ax_hist.plot(x_dense, pdf_dense_scaled, 'r-', lw=2,
-                     label=f'Normal fit ($\\mu={mu:.2f}$, $\\sigma={sigma:.2f}$)')
-            ax_hist.set_title('Histogram')
-            ax_hist.legend(fontsize=7,)
+        ax_hist.plot(x_dense, pdf_dense_scaled, 'r-', lw=2,
+                    label=f'Normal fit ($\\mu={mu:.2f}$, $\\sigma={sigma:.2f}$)')
+        ax_hist.set_title('Histogram')
+        ax_hist.legend(fontsize=7,)
 
-            # Box Plot
-            sns.boxplot(x=non_na, ax=ax_box).set(xlabel=None)
-            ax_box.set_title("Box Plot")
+        # Box Plot
+        sns.boxplot(x=plot_vals, ax=ax_box).set(xlabel=None)
+        ax_box.set_title("Box Plot")
 
-            # Violin Plot
-            sns.violinplot(x=non_na, ax=ax_violin).set(xlabel=None)
-            ax_violin.set_title('Violin Plot')
+        # Violin Plot
+        sns.violinplot(x=plot_vals, ax=ax_violin).set(xlabel=None)
+        ax_violin.set_title('Violin Plot')
 
-            # Swarm Plot
-            sns.swarmplot(x=non_na, ax=ax_swarm).set(xlabel=None)
-            ax_swarm.set_title('Swarm Plot')
+        # Swarm Plot
+        sns.swarmplot(x=plot_vals, ax=ax_swarm).set(xlabel=None)
+        ax_swarm.set_title('Swarm Plot')
 
-            plt.subplots_adjust(hspace=0.25)
-            fig.suptitle(f"{col} Visualizations")
-            plt.show()
-
-    descriptive_df = pd.DataFrame(descriptive_dict)
+        plt.subplots_adjust(hspace=0.25)
+        fig.suptitle(f"{col} Visualizations")
+        plt.show()
     
-    csv_title_li = [f"{i+1}.{col}" for i,col in enumerate(cols)]
-    csv_title = ""
-
-    for i_arg in csv_title_li:
-        csv_title += f"{i_arg}-"
-    
-    try:
-        descriptive_df.to_csv(rf'multivariate_analysis\{csv_title}descriptives.csv', index=False)
-    except Exception as e:
-        return(f"An error has occured:{e}")   
-    
-
-
 ############
 
 def multivariate_visualizations(*cols, hue_col=HUE_COL, size_col=SIZE_COL, palette=PALETTE):
